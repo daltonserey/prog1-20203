b326093b1135b30877e5a61c7329125e3423e1a2
Dalton Serey <daltonserey@gmail.com>
2020-09-10 05:38:09 -0300

    adiciona exemplos python da aula2

---
+++ aula2/p1.py
@@ -0,0 +1,4 @@
+num1 = 10 ** 2 + 15
+num2 = 3 / 5 ** 10
+num3 = num1 + num2 
+print(num1, num2, num3)
---
+++ aula2/p1b.py
@@ -0,0 +1,6 @@
+num1 = 10 ** 2 + 15
+num2 = 3 / 5 ** 10
+num3 = num1 + num2 
+print(num1)
+print(num2)
+print(num3)
